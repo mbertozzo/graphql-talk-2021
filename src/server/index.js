ce4c1c974d6646b587d23b713211c2e340eb35e6
Matteo Bertozzo <matteobertozzo@outlook.com>
2021-04-17 19:22:58 +0200

    Add two more queries [array & args]

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,54 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, UserInputError, gql } = require('apollo-server-express');
 
 const os = require('os');
 
-const typeDefs = `
+const users = require('./users');
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+  }
   type Query {
     getUsername: String
+    users: [User]
+    user(id: ID!): User
   }
 `;
 
 const resolvers = {
   Query: {
     getUsername: () => os.userInfo().username,
+    users: () => users,
+    user: (parent, { id }, context, info) => {
+      if (!id || Number.isNaN(parseInt(id, 10))) {
+        throw new UserInputError(
+          'Invalid argument value',
+          /* We can enrich our error with whatever info we want,
+             uncomment the following:
+             , {
+               pippo: 'ALOHA',
+             }
+          */
+        );
+      }
+
+      return users.find((u) => {
+        return u.id === parseInt(id, 10);
+      });
+    },
   },
 };
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  // formatError: error => {
+  //   console.log(error);
+  //   return new Error('Internal server error');
+  // },
 });
 
 const app = express();
