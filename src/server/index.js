5eebf18cfd396f51233654f23cef910cd73392b3
Matteo Bertozzo <matteobertozzo@outlook.com>
2021-04-17 18:31:23 +0200

    Add apolloServer & very first query

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
 const os = require('os');
+
+const typeDefs = `
+  type Query {
+    getUsername: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUsername: () => os.userInfo().username,
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const app = express();
 
+apolloServer.applyMiddleware({ app });
+
 app.use(express.static('dist'));
 
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/getUsername', (req, res) =>
+  res.send({ username: os.userInfo().username }),
+);
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 8080, () =>
+  console.log(
+    `🚀 Server is running at http://localhost:${process.env.PORT || 8080}`,
+  ),
+);
