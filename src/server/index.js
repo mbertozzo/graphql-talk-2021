24cc52dd3c99abf09b853d0a07d8eb9e617b2233
Matteo Bertozzo <matteobertozzo@outlook.com>
2021-04-18 15:08:01 +0200

    Add full schema for users

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,31 @@ const os = require('os');
 const users = require('./users');
 
 const typeDefs = gql`
+  type Address {
+    street: String!
+    suite: String!
+    city: String!
+    zipcode: Int!
+    geo: Geo
+  }
+  type Geo {
+    lat: String!
+    lng: String!
+  }
+  type Company {
+    name: String!
+    catchPhrase: String!
+    bs: String!
+  }
   type User {
     id: ID!
     name: String!
     username: String!
+    email: String!
+    address: Address
+    phone: String!
+    website: String!
+    company: Company
   }
   type Query {
     getUsername: String
--- src/server/users.js
+++ src/server/users.js
@@ -8,7 +8,7 @@ module.exports = [
       street: 'Kulas Light',
       suite: 'Apt. 556',
       city: 'Gwenborough',
-      zipcode: '92998-3874',
+      zipcode: 92998,
       geo: {
         lat: '-37.3159',
         lng: '81.1496',
@@ -31,7 +31,7 @@ module.exports = [
       street: 'Victor Plains',
       suite: 'Suite 879',
       city: 'Wisokyburgh',
-      zipcode: '90566-7771',
+      zipcode: 90566,
       geo: {
         lat: '-43.9509',
         lng: '-34.4618',
@@ -54,7 +54,7 @@ module.exports = [
       street: 'Douglas Extension',
       suite: 'Suite 847',
       city: 'McKenziehaven',
-      zipcode: '59590-4157',
+      zipcode: 59590,
       geo: {
         lat: '-68.6102',
         lng: '-47.0653',
@@ -77,7 +77,7 @@ module.exports = [
       street: 'Hoeger Mall',
       suite: 'Apt. 692',
       city: 'South Elvis',
-      zipcode: '53919-4257',
+      zipcode: 53919,
       geo: {
         lat: '29.4572',
         lng: '-164.2990',
@@ -100,7 +100,7 @@ module.exports = [
       street: 'Skiles Walks',
       suite: 'Suite 351',
       city: 'Roscoeview',
-      zipcode: '33263',
+      zipcode: 33263,
       geo: {
         lat: '-31.8129',
         lng: '62.5342',
@@ -123,7 +123,7 @@ module.exports = [
       street: 'Norberto Crossing',
       suite: 'Apt. 950',
       city: 'South Christy',
-      zipcode: '23505-1337',
+      zipcode: 23505,
       geo: {
         lat: '-71.4197',
         lng: '71.7478',
@@ -146,7 +146,7 @@ module.exports = [
       street: 'Rex Trail',
       suite: 'Suite 280',
       city: 'Howemouth',
-      zipcode: '58804-1099',
+      zipcode: 58804,
       geo: {
         lat: '24.8918',
         lng: '21.8984',
@@ -169,7 +169,7 @@ module.exports = [
       street: 'Ellsworth Summit',
       suite: 'Suite 729',
       city: 'Aliyaview',
-      zipcode: '45169',
+      zipcode: 45169,
       geo: {
         lat: '-14.3990',
         lng: '-120.7677',
@@ -192,7 +192,7 @@ module.exports = [
       street: 'Dayna Park',
       suite: 'Suite 449',
       city: 'Bartholomebury',
-      zipcode: '76495-3109',
+      zipcode: 76495,
       geo: {
         lat: '24.6463',
         lng: '-168.8889',
@@ -215,7 +215,7 @@ module.exports = [
       street: 'Kattie Turnpike',
       suite: 'Suite 198',
       city: 'Lebsackbury',
-      zipcode: '31428-2261',
+      zipcode: 31428,
       geo: {
         lat: '-38.2386',
         lng: '57.2232',
