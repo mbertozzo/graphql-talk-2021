00d05dc6ab9affa702d6047e64bf7ef84b83d6d4
Matteo Bertozzo <matteobertozzo@outlook.com>
2021-04-25 18:36:59 +0200

    Add first ApolloClient integration

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
 import App from './App';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        columns {
+          id
+          title
+          position
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
