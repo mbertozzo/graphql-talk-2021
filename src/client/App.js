0c55e75a0f010ca66dda5e72450c1cbfc9dbd526
Matteo Bertozzo <matteobertozzo@outlook.com>
2021-04-25 18:26:05 +0200

    Add FE only solution

--- src/client/App.js
+++ src/client/App.js
@@ -1,28 +1,121 @@
-import React, { Component } from 'react';
-import './app.scss';
-import GraphQL from './graphql.png';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
-export default class App extends Component {
-  state = { username: null };
+import dataset from './dataset';
+import Column from './Column';
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then((res) => res.json())
-      .then((user) => this.setState({ username: user.username }));
-  }
+const Container = styled.div`
+  display: flex;
+`;
 
-  render() {
-    const { username } = this.state;
+const App = (props) => {
+  const [data, setData] = useState(dataset);
 
-    return (
-      <div>
-        {username ? (
-          <h1>{`Hello ${username}!`}</h1>
-        ) : (
-          <h1>Loading.. please wait!</h1>
+  const onDragEnd = (result) => {
+    console.log('RESULT', result);
+    const { destination, source, draggableId, type } = result;
+    // If there is no destination
+    if (!destination) {
+      return;
+    }
+
+    // If source and destination is the same
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // If you're dragging columns
+    if (type === 'column') {
+      const newColumnOrder = Array.from(data.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+      const newState = {
+        ...data,
+        columnOrder: newColumnOrder,
+      };
+      setData(newState);
+      return;
+    }
+
+    // Anything below this happens if you're dragging tasks
+    const start = data.columns[source.droppableId];
+    const finish = data.columns[destination.droppableId];
+
+    // If dropped inside the same column
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+      const newState = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+      setData(newState);
+      return;
+    }
+
+    // If dropped in a different column
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+
+    setData(newState);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="all-columns" direction="horizontal" type="column">
+        {(provided) => (
+          <Container {...provided.droppableProps} ref={provided.innerRef}>
+            {data.columnOrder.map((id, index) => {
+              const column = data.columns[id];
+              const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+
+              return (
+                <Column
+                  key={column.id}
+                  column={column}
+                  tasks={tasks}
+                  index={index}
+                />
+              );
+            })}
+            {provided.placeholder}
+          </Container>
         )}
-        <img src={GraphQL} alt="react" />
-      </div>
-    );
-  }
-}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default App;
